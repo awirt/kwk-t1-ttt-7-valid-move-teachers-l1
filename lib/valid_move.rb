32acce339c2662f49b4eb9342fa49bdc47bd41da
Andrew Wirt <awjwirt@gmail.com>
2018-05-14 04:55:40 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,9 +9,7 @@ end
 
 
 def valid_move?(board, index)
-
-  if index.between?(0,8) && !(position_taken?(board,index))
-    return true
+  if index.between?(0,8)
+    !(position_taken?(board,index))
   end
-  
 end
